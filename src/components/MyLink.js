b6a64ae53e6c20f45c06fd5ec4dfd266ed61cbc8
Ibrahim Albrahim <hemo7f12@gmail.com>
2023-03-14 21:13:54 +0300

    add blog's Instagram link

--- src/components/MyLink.js
+++ src/components/MyLink.js
@@ -6,14 +6,15 @@ import { faStore, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 function MyLink() {
     const links = [
-        { text: 'AHHSN Shop | متجر احسن',   icon: faStore,          href: 'https://shop.ahhsn.com/Ar' },
-        { text: 'Call Us | اتصل بنا',       icon: faPhone,          href: 'tel:0503727124' },
-        { text: 'Whatsapp | واتساب',        icon: faWhatsapp,       href: 'https://wa.me/966503727124' },
-        { text: 'Instagram | انستقرام',     icon: faInstagram,      href: 'http://instagram.com/ahhsnshop' },
-        { text: 'Snapchat | سنابشات',       icon: faSnapchatSquare, href: 'https://www.snapchat.com/add/ahhsnshop' },
-        { text: 'TikTok | تيك توك',         icon: faTiktok,         href: 'https://www.tiktok.com/@ahhsnshop' },
-        { text: 'Twitter | تويتر',          icon: faTwitter,        href: 'https://twitter.com/ahhsnshop' },
-        { text: 'AHHSN Blog | مدونة احسن',  icon: faWordpress,      href: 'https://ahhsn.com/' },
+        { text: 'AHHSN Shop | متجر احسن',                   icon: faStore,          href: 'https://shop.ahhsn.com/Ar' },
+        { text: 'Call Us | اتصل بنا',                       icon: faPhone,          href: 'tel:0503727124' },
+        { text: 'Whatsapp | واتساب',                        icon: faWhatsapp,       href: 'https://wa.me/966503727124' },
+        { text: 'Instagram | انستقرام',                     icon: faInstagram,      href: 'http://instagram.com/ahhsnshop' },
+        { text: 'Snapchat | سنابشات',                       icon: faSnapchatSquare, href: 'https://www.snapchat.com/add/ahhsnshop' },
+        { text: 'TikTok | تيك توك',                         icon: faTiktok,         href: 'https://www.tiktok.com/@ahhsnshop' },
+        { text: 'Twitter | تويتر',                          icon: faTwitter,        href: 'https://twitter.com/ahhsnshop' },
+        { text: 'AHHSN Blog | مدونة احسن',                  icon: faWordpress,      href: 'https://ahhsn.com/' },
+        { text: "Blog's Instagram | انستقرام المدونة",     icon: faInstagram,      href: 'http://instagram.com/ahhsncom' },
     ]
     return (
         links.map(link =>
