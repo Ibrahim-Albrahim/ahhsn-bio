3aea559909ce7e81eaca3119d5970d7467edcc69
Ibrahim Albrahim <hemo7f12@gmail.com>
2023-03-15 03:21:57 +0300

    redirect all wrong paths to main page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 import './assets/scss/index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/bio" element={<App />} />
+      <Route path="*" element={<Navigate to="/bio" />} />
+    </Routes>
+  </BrowserRouter>
 );
